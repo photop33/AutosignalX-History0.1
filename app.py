75f78547c0fb265c5c6490daca62ca012b566208
Liorsw <kniotop@gmail.com>
2025-07-06 23:32:06 +0300

    lior 1

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ st.set_page_config(page_title="📊 AutoSignalX-History App", layout="wide")
 st.title("👋 ברוכים הבאים למערכת AutoSignalX")
 
 
+
+
+##
+
 st.markdown("בחר עמוד מהתפריט בצד שמאל כדי להתחיל ⬅️")
