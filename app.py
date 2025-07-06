fb1942da139e5e19d87b7bdae3124f229e923af2
Liorsw <kniotop@gmail.com>
2025-07-06 23:32:34 +0300

    lior 2

--- app.py
+++ app.py
@@ -3,9 +3,5 @@ import streamlit as st
 st.set_page_config(page_title="📊 AutoSignalX-History App", layout="wide")
 st.title("👋 ברוכים הבאים למערכת AutoSignalX")
 
-
-
-
-##
-
+##34
 st.markdown("בחר עמוד מהתפריט בצד שמאל כדי להתחיל ⬅️")
