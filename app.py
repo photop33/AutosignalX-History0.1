ce8972f54ad6b31f462cdb30cc6823d3771dfb46
Liorsw <kniotop@gmail.com>
2025-07-06 23:29:23 +0300

    yona yagever

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ import streamlit as st
 st.set_page_config(page_title="📊 AutoSignalX-History App", layout="wide")
 st.title("👋 ברוכים הבאים למערכת AutoSignalX")
 
+
 st.markdown("בחר עמוד מהתפריט בצד שמאל כדי להתחיל ⬅️")
