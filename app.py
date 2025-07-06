f39807e37156b73f06a8ccaf6638f4fce630ebdc
photop33 <75907575+photop33@users.noreply.github.com>
2025-07-06 23:38:59 +0300

    Lior (#2)
    * lior 1
    
    * lior 2
    
    * yona fix

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.set_page_config(page_title="📊 AutoSignalX-History App", layout="wide")
 st.title("👋 ברוכים הבאים למערכת AutoSignalX")
 
-
+##35
 st.markdown("בחר עמוד מהתפריט בצד שמאל כדי להתחיל ⬅️")
