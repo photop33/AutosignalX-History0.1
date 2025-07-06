cf7e2e7786d16805c3565e5ddea6cf8998fb7bee
Liorsw <kniotop@gmail.com>
2025-07-06 23:34:37 +0300

    yona fix

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.set_page_config(page_title="📊 AutoSignalX-History App", layout="wide")
 st.title("👋 ברוכים הבאים למערכת AutoSignalX")
 
-##34
+##35
 st.markdown("בחר עמוד מהתפריט בצד שמאל כדי להתחיל ⬅️")
